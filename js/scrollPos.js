d96124e4ef128489b87b4708abb8958a9cdc2e2a
adrianiannantuono <aiannantuono@me.com>
2020-07-03 14:09:07 -0400

    Update scrollPos.js

--- js/scrollPos.js
+++ js/scrollPos.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
     var doc = document.documentElement;
     var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-    if(top <= 99) {
+    if(top >= 100) {
        jQuery("#top-navigation").css('background-color', 'rgba(56, 56, 56, 0.9)');
        jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
     }
