19796e508e07a06d404a79ac06e25737048f6d3b
adrianiannantuono <aiannantuono@me.com>
2020-07-03 14:08:25 -0400

    Update scrollPos.js

--- js/scrollPos.js
+++ js/scrollPos.js
@@ -3,8 +3,10 @@ window.onscroll = function() {
     var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
     if(top <= 99) {
        jQuery("#top-navigation").css('background-color', 'rgba(56, 56, 56, 0.9)');
+       jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
     }
     else {
        jQuery("#top-navigation").css('background', 'none');
+       jQuery("#top-navigation").css('transition', 'ease-in-out 0.25s');
     }
 }
