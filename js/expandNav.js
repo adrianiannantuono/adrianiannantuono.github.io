28cf39c0bca3ca4ed70263cbd8401265b0477f9b
adrianiannantuono <aiannantuono@me.com>
2020-07-06 15:54:19 -0400

    Update expandNav.js

--- js/expandNav.js
+++ js/expandNav.js
@@ -1,4 +1,5 @@
 function expandNav() {
   jQuery(".top-button").css('display', 'flex');
+  jQuery("#top-navigation").css('height', '1vh');
   jQuery("#top-navigation").css('flex-direction', 'column');
 }
