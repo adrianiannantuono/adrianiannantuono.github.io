0496c4bab1fd0cf7dbe8842d50d23d481493a3f2
adrianiannantuono <aiannantuono@me.com>
2020-07-06 16:08:08 -0400

    Update expandNav.js

--- js/expandNav.js
+++ js/expandNav.js
@@ -1,5 +1,5 @@
 function expandNav() {
-  if (document.getElementById('.top-navigation').clientHeight != "100vh") {
+  if (document.getElementById('#top-navigation').clientHeight != "100vh") {
     jQuery("#top-navigation").css('background-color', 'rgba(56, 56, 56, 0.95)');
     jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
     jQuery(".top-button").css('display', 'flex');
