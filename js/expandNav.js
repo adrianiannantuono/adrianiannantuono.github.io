637533254c86dfed955bc0f1fe469d6e1d41c510
adrianiannantuono <aiannantuono@me.com>
2020-07-06 16:00:52 -0400

    a

--- js/expandNav.js
+++ js/expandNav.js
@@ -1,5 +1,5 @@
 function expandNav() {
   jQuery(".top-button").css('display', 'flex');
-  jQuery("#top-navigation").css('height', '1vh');
-  jQuery("#top-navigation").css('flex-direction', 'column');
+  jQuery(".top-navigation").css('height', '100vh');
+  jQuery(".top-navigation").css('flex-direction', 'column');
 }
