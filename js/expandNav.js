12060fae796179449938b7df79af43884bdc3cc2
adrianiannantuono <aiannantuono@me.com>
2020-07-06 16:07:09 -0400

    Update expandNav.js

--- js/expandNav.js
+++ js/expandNav.js
@@ -1,5 +1,15 @@
 function expandNav() {
-  jQuery(".top-button").css('display', 'flex');
-  jQuery(".top-navigation").css('height', '100vh');
-  jQuery(".top-navigation").css('flex-direction', 'column');
+  if (document.getElementById('.top-navigation').clientHeight != "100vh") {
+    jQuery("#top-navigation").css('background-color', 'rgba(56, 56, 56, 0.95)');
+    jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
+    jQuery(".top-button").css('display', 'flex');
+    jQuery(".top-navigation").css('height', '100vh');
+    jQuery(".top-navigation").css('flex-direction', 'column');
+  } else {
+    jQuery("#top-navigation").css('background-color', 'none');
+    jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
+    jQuery(".top-button").css('display', 'flex');
+    jQuery(".top-navigation").css('height', '100vh');
+    jQuery(".top-navigation").css('flex-direction', 'column');
+  }
 }
