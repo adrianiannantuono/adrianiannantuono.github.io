53e2f934b6110611be14b87e317b3463937e45df
adrianiannantuono <aiannantuono@me.com>
2020-07-07 12:16:31 -0400

    a

--- js/expandNav.js
+++ js/expandNav.js
@@ -2,6 +2,7 @@ function expandNav() {
   if (document.getElementById('top-navigation').style.height != "100vh") {
     jQuery("#top-navigation").css('background-color', 'rgba(56, 56, 56, 0.95)');
     jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
+    jQuery(".top-button").css('display', 'flex');
     jQuery(".top-button").css('height', '90px');
     jQuery("#home").css('margin-left', 'inherit');
     jQuery(".top-navigation").css('height', '100vh');
@@ -11,6 +12,7 @@ function expandNav() {
     jQuery("#top-navigation").css('background', 'none');
     jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
     jQuery("#home").css('margin-left', 'auto');
+    jQuery(".top-button").css('display', 'none');
     jQuery(".top-button").css('height', 'auto');
     jQuery(".top-navigation").css('flex-direction', 'row');
     jQuery(".top-navigation").css('height', 'inherit');
