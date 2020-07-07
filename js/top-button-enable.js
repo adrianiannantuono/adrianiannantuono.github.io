276b1e421ad548f33bbd3ed7f172813d987af212
adrianiannantuono <aiannantuono@me.com>
2020-07-07 12:09:22 -0400

    a

--- js/top-button-enable.js
+++ js/top-button-enable.js
@@ -9,5 +9,13 @@ if (document.getElementById('top-navigation').style.height == "100vh") {
         jQuery(".top-hamburger").css('display', 'flex');
       }
 } else {
-
+    if ($(window).width() > 1000) {
+        jQuery(".top-button").css('display', 'flex');
+        jQuery(".top-hamburger").css('display', 'none');
+        console.log("A");
+      } else {
+        console.log("HERE");
+        jQuery(".top-button").css('display', 'none');
+        jQuery(".top-hamburger").css('display', 'flex');
+      }
 }
