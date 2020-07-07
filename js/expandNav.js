52e80bd4a014e549c0269de2ab4e074c9d77f940
adrianiannantuono <aiannantuono@me.com>
2020-07-07 11:12:03 -0400

    a

--- js/expandNav.js
+++ js/expandNav.js
@@ -6,6 +6,9 @@ function expandNav() {
     jQuery("#home").css('margin-left', 'inherit');
     jQuery(".top-navigation").css('height', '100vh');
     jQuery(".top-navigation").css('flex-direction', 'column');
+    jQuery(".top-hamburger").css('position', 'absolute');
+    Query(".top-hamburger").css('top', '50px');
+    Query(".top-hamburger").css('right', '50px');
     jQuery("*").css('overflow', 'hidden');
   } else {
     jQuery("#top-navigation").css('background-color', 'none');
@@ -14,6 +17,7 @@ function expandNav() {
     jQuery(".top-button").css('display', 'flex');
     jQuery(".top-navigation").css('flex-direction', 'row');
     jQuery(".top-navigation").css('height', 'inherit');
+    jQuery(".top-hamburger").css('position', 'static');
     jQuery("*").css('overflow-x', 'hidden');
     jQuery("*").css('overflow-y', 'auto');
   }
