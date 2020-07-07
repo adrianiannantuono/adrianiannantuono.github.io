dd8d6264976fc13d03392c0af91a39aed17c3d35
adrianiannantuono <aiannantuono@me.com>
2020-07-07 12:29:24 -0400

    a

--- js/top-button-enable.js
+++ js/top-button-enable.js
@@ -1,21 +1,23 @@
-if (document.getElementById('top-navigation').style.height == "100vh") {
-    if ($(window).width() > 1000) {
-        jQuery(".top-button").css('display', 'flex');
-        jQuery(".top-hamburger").css('display', 'none');
-        console.log("A");
-      } else {
-        console.log("HERE");
-        jQuery(".top-button").css('display', 'none');
-        jQuery(".top-hamburger").css('display', 'flex');
-      }
-} else {
-    if ($(window).width() > 1000) {
-        jQuery(".top-button").css('display', 'flex');
-        jQuery(".top-hamburger").css('display', 'none');
-        console.log("A");
-      } else {
-        console.log("HERE");
-        jQuery(".top-button").css('display', 'none');
-        jQuery(".top-hamburger").css('display', 'flex');
-      }
-}
+$(window).resize(function() {
+    if (document.getElementById('top-navigation').style.height == "100vh") {
+        if ($(window).width() > 1000) {
+            jQuery(".top-button").css('display', 'flex');
+            jQuery(".top-hamburger").css('display', 'none');
+            console.log("A");
+          } else {
+            console.log("HERE");
+            jQuery(".top-button").css('display', 'none');
+            jQuery(".top-hamburger").css('display', 'flex');
+          }
+    } else {
+        if ($(window).width() > 1000) {
+            jQuery(".top-button").css('display', 'flex');
+            jQuery(".top-hamburger").css('display', 'none');
+            console.log("A");
+          } else {
+            console.log("HERE");
+            jQuery(".top-button").css('display', 'none');
+            jQuery(".top-hamburger").css('display', 'flex');
+          }
+    }
+}).resize();
