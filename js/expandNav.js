53511e67844b837b1b41c17c7bee1705a4888d85
adrianiannantuono <aiannantuono@me.com>
2020-07-07 11:33:56 -0400

    a

--- js/expandNav.js
+++ js/expandNav.js
@@ -9,6 +9,9 @@ function expandNav() {
     jQuery(".top-navigation").css('flex-direction', 'column');
     jQuery("*").css('overflow', 'hidden');
   } else {
+    if (window.screen.width > 1000) {
+      jQuery(".top-hamburger").css('display', 'none');
+    }
     jQuery("#top-navigation").css('background-color', 'none');
     jQuery("#top-navigation").css('transition', 'ease-in-out 0.4s');
     jQuery("#home").css('margin-left', 'auto');
