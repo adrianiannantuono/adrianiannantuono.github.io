bb8c7495485c85d96deede38258ba993cfb082d6
adrianiannantuono <aiannantuono@me.com>
2020-07-07 11:47:43 -0400

    a

--- js/expandNav.js
+++ js/expandNav.js
@@ -12,7 +12,9 @@ function expandNav() {
     if (window.screen.width > 1000) {
       jQuery(".top-button").css('display', 'flex');
       jQuery(".top-hamburger").css('display', 'none');
+      console.log("A");
     } else {
+      console.log("HERE");
       jQuery(".top-button").css('display', 'none');
       jQuery(".top-hamburger").css('display', 'flex');
     }
